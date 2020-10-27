8bf58ccad23f165511c4d001bd4e145f18bd4307
Adabe <adabe@scienta.co>
2020-10-27 20:10:24 +0100

    style categories page

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app_tutorial/providers/dummy_data.dart';
+import 'package:meal_app_tutorial/widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("Meal Categories"),),
       body: GridView(
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
@@ -11,6 +14,9 @@ class CategoriesScreen extends StatelessWidget {
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat.title, cat.color);
+        }).toList(),
       ),
     );
   }
