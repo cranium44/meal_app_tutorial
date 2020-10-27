5eb783554aa0f7764dcd02d3bf38f9efab3e07fb
Adabe <adabe@scienta.co>
2020-10-27 22:38:34 +0100

    Style each category meal

--- lib/screens/meals/category_meals_screen.dart
+++ lib/screens/meals/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app_tutorial/providers/dummy_data.dart';
+import 'package:meal_app_tutorial/widgets/category_meals_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -17,7 +18,7 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return CategoryMealItem(categoryMeals[index]);
         },
         itemCount: categoryMeals.length,
       ),
